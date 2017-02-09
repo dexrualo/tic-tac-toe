fb9cd3873f8cb1fcf4f33e12dc1c492078d2bd84
Dexter Rualo <dexterrualo@gmail.com>
2017-02-09 10:07:04 +0800

    Added components and some style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
   render() {
     return (
       <div className="App">
@@ -13,6 +24,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Board 
+          squares={this.state.squares}
+          onClick={(i) => this.handleClick(i)}
+        />
       </div>
     );
   }
@@ -20,23 +35,25 @@ class App extends Component {
 
 function Square (props) {
   return (
-    <div onClick={props.onClick}>{props.symbol}</div>
+    <div className='square' onClick={() => props.onClick()}>{props.value}</div>
   );
 }
 
 class Board extends Component {
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(9).fill(null)
-    }
-  }
-  handleClick() {
-
-  }
   render() {
+    const squares = this.props.squares;
     return (
-      <Square value={this.state.squares[i]} onClick={() => this.setState({value: 'X'})}></Square>
+      <div className='board'>
+        <Square value={squares[0]} onClick={() => this.props.onClick(0)} />
+        <Square value={squares[1]} onClick={() => this.props.onClick(1)} />
+        <Square value={squares[2]} onClick={() => this.props.onClick(2)} />
+        <Square value={squares[3]} onClick={() => this.props.onClick(3)} />
+        <Square value={squares[4]} onClick={() => this.props.onClick(4)} />
+        <Square value={squares[5]} onClick={() => this.props.onClick(5)} />
+        <Square value={squares[6]} onClick={() => this.props.onClick(6)} />
+        <Square value={squares[7]} onClick={() => this.props.onClick(7)} />
+        <Square value={squares[8]} onClick={() => this.props.onClick(8)} />
+      </div>
     );
   }
 }
